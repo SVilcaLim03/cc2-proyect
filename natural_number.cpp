b23ffc1ff8e5445419c7d3fb8d7f8634bc6842ed
lpradop <lpradop@unsa.edu.pe>
2020-06-26 19:50:13 -0500

    google formatting

--- natural_number.cpp
+++ natural_number.cpp
@@ -1,77 +1,68 @@
 #include "natural_number.hpp"
+
 #include <iostream>
-NaturalNumber::NaturalNumber(const long long number) : number_(number)
-{
-    if (number < 0)
-    {
-        number_ = 0;
-    }
+
+NaturalNumber::NaturalNumber(const long long int number) : number_(number) {
+  if (number < 0) {
+    number_ = 0;
+  }
 }
-NaturalNumber NaturalNumber::operator++() //ok
-{
-    ++number_;
+
+NaturalNumber::operator int() const { return (int)number_; }
+
+NaturalNumber NaturalNumber::operator++() {
+  ++number_;
+  return *this;
+}
+
+NaturalNumber NaturalNumber::operator++(int) {
+  long long tmp = number_;
+  ++tmp;
+  return NaturalNumber(tmp);
+}
+
+NaturalNumber NaturalNumber::operator--() {
+  if (number_ != 0) {
+    --number_;
+  }
+  return *this;
+}
+
+NaturalNumber NaturalNumber::operator--(int) {
+  if (number_ == 0) {
     return *this;
-}
-NaturalNumber NaturalNumber::operator++(int) //ok
-{
+  } else {
     long long tmp = number_;
-    ++tmp;
+    --tmp;
     return NaturalNumber(tmp);
-}
-NaturalNumber NaturalNumber::operator--() //ok
-{
-    if (number_ != 0)
-    {
-        --number_;
-    }
-
-    return *this;
-}
-NaturalNumber NaturalNumber::operator--(int) //ok
-{
-    if (number_ == 0)
-    {
-        return *this;
-    }
-    else
-    {
-        long long tmp = number_;
-        --tmp;
-        return NaturalNumber(tmp);
-    }
-}
-NaturalNumber &NaturalNumber::operator+=(const NaturalNumber &other)
-{
-    number_ += other.number_;
-    return *this;
-}
-std::ostream &operator<<(std::ostream &ostream, const NaturalNumber &obj)
-{
-    ostream << obj.number_;
-    return ostream;
+  }
 }
 
-NaturalNumber operator+(NaturalNumber a, const NaturalNumber &b)
-{
-    a += b;
-    return a;
+NaturalNumber& NaturalNumber::operator+=(const NaturalNumber& other) {
+  number_ += other.number_;
+  return *this;
 }
 
-int operator+(const NaturalNumber a, const int b)
-{
-    return (a.number_ + b);
-}
-int operator+(const int a, const NaturalNumber b)
-{
-    return (b.number_ + a);
-}
-float operator+(const NaturalNumber a, const float b)
-{
-    return (a.number_ + b);
-}
-float operator+(const float a, const NaturalNumber b)
-{
-    return (b.number_ + a);
+std::ostream& operator<<(std::ostream& ostream, const NaturalNumber& obj) {
+  ostream << obj.number_;
+  return ostream;
 }
 
-//todo cast operators, mod operator
+NaturalNumber operator+(NaturalNumber a, const NaturalNumber& b) {
+  a += b;
+  return a;
+}
+
+int operator+(const NaturalNumber a, const int b) { return (a.number_ + b); }
+
+int operator+(const int a, const NaturalNumber b) { return (b.number_ + a); }
+
+float operator+(const NaturalNumber a, const float b) {
+  return (a.number_ + b);
+}
+
+float operator+(const float a, const NaturalNumber b) {
+  return (b.number_ + a);
+}
+
+// todo cast operators, mod operator
--- natural_number.hpp
+++ natural_number.hpp
@@ -1,32 +1,24 @@
-//Natural Numbers representation for moar typesafety
+// Natural Numbers representation for moar typesafety
 #pragma once
 #include <iostream>
-class NaturalNumber
-{
-private:
-    unsigned long long number_;
+class NaturalNumber {
+ private:
+  unsigned long long int number_;
 
-public:
-    NaturalNumber(const long long number);
-    NaturalNumber operator++();
-    NaturalNumber operator++(int);
-    NaturalNumber operator--();
-    NaturalNumber operator--(int);
-    NaturalNumber &operator+=(const NaturalNumber &other);
-    template <typename CastType>
-    operator CastType() const;
+ public:
+  NaturalNumber(const long long int number = 0);
+  NaturalNumber operator++();
+  NaturalNumber operator++(int);
+  NaturalNumber operator--();
+  NaturalNumber operator--(int);
+  NaturalNumber& operator+=(const NaturalNumber& other);
+  operator int() const;
 
-    friend int operator+(const NaturalNumber a, const int b);
-    friend int operator+(const int a, const NaturalNumber b);
-    friend float operator+(const NaturalNumber a, const float b);
-    friend float operator+(const float a, const NaturalNumber b);
-    friend NaturalNumber operator+(NaturalNumber a,const NaturalNumber &b);
-    friend std::ostream& operator<<(std::ostream& os, const NaturalNumber& number);
-    
+  friend int operator+(const NaturalNumber a, const int b);
+  friend int operator+(const int a, const NaturalNumber b);
+  friend float operator+(const NaturalNumber a, const float b);
+  friend float operator+(const float a, const NaturalNumber b);
+  friend NaturalNumber operator+(NaturalNumber a, const NaturalNumber& b);
+  friend std::ostream& operator<<(std::ostream& ostream,
+                                  const NaturalNumber& number);
 };
-
-template <typename CastType>//maybe restrict type cast?
-NaturalNumber::operator CastType() const
-{
-    return (CastType)number_;
-}
