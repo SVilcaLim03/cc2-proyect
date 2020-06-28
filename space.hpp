04d590920f2aeaf658f03b2220d32d5c6d824815
lpradop <lpradop@unsa.edu.pe>
2020-06-28 09:44:32 -0500

    merged changes

--- space.hpp
+++ space.hpp
@@ -1,14 +1,9 @@
 #pragma once
 #include <iostream>
 #include <map>
-<<<<<<< HEAD
 #include "natural_number.hpp"
-
-=======
-
 #include "entity.hpp"
-#include "natural_number.hpp"
->>>>>>> 87d78cb3157ff5f3e5a813123324035a2692f957
+
 class Space {
  private:
   using Identifier = NaturalNumber;
@@ -18,11 +13,7 @@ class Space {
 
  public:
   Location GetLocation(Entity entity) const;
-<<<<<<< HEAD
-  bool CheckValidMove(entity : Entity, location : Location) const;
-=======
-  //   bool CheckValidMove(entity : Entity, location : Location) const;
->>>>>>> 87d78cb3157ff5f3e5a813123324035a2692f957
+  bool CheckValidMove(Entity entity, Location location) const;
   Space();
   ~Space();
 };
