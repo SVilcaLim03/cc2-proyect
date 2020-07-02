dc25a0f631a7e0875c65a265f286845a1a853833
lpradop <lpradop@unsa.edu.pe>
2020-07-01 23:48:45 -0500

    remove not used headers

--- date.cpp
+++ date.cpp
@@ -1,11 +1,10 @@
 #include "date.hpp"
 
-#include <iostream>
-
 #include "natural_number.hpp"
 #include "time.hpp"
 
 Date::Date() : second_(0), minute_(0), hour_(0), raw_date_(0) {}
+Date::~Date() {}
 
 Date::Date(Time& time) {
   raw_date_ = time.GetCurrentTime();
@@ -24,11 +23,6 @@ Date::Date(NaturalNumber hour, NaturalNumber minute, NaturalNumber second)
 }
 
 NaturalNumber Date::GetRawDate() { return raw_date_; }
-
 NaturalNumber Date::GetHour() { return hour_; }
-
 NaturalNumber Date::GetMinute() { return minute_; }
-
 NaturalNumber Date::GetSecond() { return second_; }
-
-Date::~Date() {}
