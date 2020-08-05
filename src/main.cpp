96ab5466b521f69a6f4ffd1bfc2cc543a0fb163d
Brocolio de la chunsa <lpradop@unsa.edu.pe>
2020-08-05 11:54:16 -0500

    added exit event

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,10 @@
 int main() {
 
   try {
+    SDL_Event event;
+    bool running = true;
     GraphicHandler graphic_handler{};
+
     Button btn_start(nullptr);
     std::map<int, std::pair<SDL_Rect, int>> btn_start_sprite_map = {
         {Button::NEUTRAL, std::make_pair(SDL_Rect{0, 0, 176, 64}, 1)},
@@ -18,10 +21,18 @@ int main() {
         {Button::CLICKED, std::make_pair(SDL_Rect{0, 128, 176, 64}, 1)}};
     Animation btn_start_animation(btn_start, "test.png", btn_start_sprite_map,
                                   graphic_handler.GetRenderer());
-    while (true) {
+
+    while (running) {
       graphic_handler.ClearRenderer();
-      graphic_handler.Render(btn_start_animation, std::make_pair(50, 50));
+      graphic_handler.Render(btn_start_animation, std::make_pair(200, 200));
       graphic_handler.UpdateScreen();
+
+      while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+        case SDL_QUIT:
+          running = false;
+        }
+      }
     }
   } catch (std::exception &e) {
     std::cout << e.what() << std::endl;
